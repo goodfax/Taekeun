d2314417b34aa987d178f45e7c2a61fa75a6a2bd
TaekEun <goodfax2000@naver.com>
2020-01-14 18:40:08 +0900

    20200114

---
+++ word2vec/kor/ko_word2vec.py
@@ -0,0 +1,12 @@
+# model generation and save
+
+from gensim.models import Word2Vec
+
+ko_fname = './data/prepro_ko_wiki.txt'
+model_fname = './model/taekeun/ko.bin'
+
+corpus = [sent.strip().split(" ") for sent in open(ko_fname, 'r', encoding='utf-8').readlines()]
+model = Word2Vec(corpus, size=100, workers=4, sg=1)
+model.save(model_fname)
+
+print('Finish')
