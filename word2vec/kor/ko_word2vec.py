f21703285a56f46501dca22a5ecc538f412184a5
TaekEun <goodfax2000@naver.com>
2020-01-14 20:47:09 +0900

    20200114

--- word2vec/kor/ko_word2vec.py
+++ word2vec/kor/ko_word2vec.py
@@ -6,7 +6,7 @@ ko_fname = './data/prepro_ko_wiki.txt'
 model_fname = './model/taekeun/ko.bin'
 
 corpus = [sent.strip().split(" ") for sent in open(ko_fname, 'r', encoding='utf-8').readlines()]
-model = Word2Vec(corpus, size=100, workers=4, sg=1)
+model = Word2Vec(corpus, size=200, workers=4, sg=1)
 model.save(model_fname)
 
 print('Finish')
