2cf823753089c969c57177f5ea6eab33d31faee2
TaekEun <goodfax2000@naver.com>
2020-01-17 15:31:53 +0900

    20200117

--- word2vec/word2vec_utils.py
+++ word2vec/word2vec_utils.py
@@ -7,6 +7,9 @@ class SentenceIterator:
     def __init__(self, filepath):
         self.filepath = filepath
 
+    # int(4763709)는 prepro_en_wiki.txt의 라인 수
+    # iterator에 크기 계산을 위한 소스를 넣으면 RAM의 용량을 초과하여
+    # 미리 계산해 직접 값을 입력함. => 개선 필요.
     def __iter__(self):
         for line in tqdm(open(self.filepath, 'r', encoding='utf-8'), total=int(4763709)):
             yield line.split()
