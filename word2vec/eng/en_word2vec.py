2cb2359040c5992895cc55a2ae507b2e693fa20c
TaekEun <goodfax2000@naver.com>
2020-01-21 11:27:51 +0900

    20200121

--- word2vec/eng/en_word2vec.py
+++ word2vec/eng/en_word2vec.py
@@ -1,17 +1,17 @@
 # model generation and save
 
-from word2vec.word2vec_utils import SentenceIterator
+import logging
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s',
+    level=logging.INFO)
+
 from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
+import multiprocessing as mp
 
 en_fname = './data/prepro_en_wiki.txt'
 model_fname = './model/taekeun/en.bin'
 
-corpus = SentenceIterator(en_fname)
+model = Word2Vec(LineSentence(en_fname), size=300, workers=mp.cpu_count(), sg=1)
 
-print('Start')
-
-# default epoch: 5
-model = Word2Vec(corpus, size=300, workers=4, sg=1)
 model.save(model_fname)
-
-print('Finish')
--- word2vec/kor/ko_word2vec.py
+++ word2vec/kor/ko_word2vec.py
@@ -1,12 +1,17 @@
 # model generation and save
 
+import logging
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s',
+    level=logging.INFO)
+
 from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
+import multiprocessing as mp
 
 ko_fname = './data/prepro_ko_wiki.txt'
 model_fname = './model/taekeun/ko.bin'
 
-corpus = [sent.strip().split(" ") for sent in open(ko_fname, 'r', encoding='utf-8').readlines()]
-model = Word2Vec(corpus, size=200, workers=4, sg=1)
+# corpus = [sent.strip().split(" ") for sent in open(ko_fname, 'r', encoding='utf-8').readlines()]
+model = Word2Vec(LineSentence(ko_fname), size=100, workers=mp.cpu_count()-1, sg=1)
 model.save(model_fname)
-
-print('Finish')
